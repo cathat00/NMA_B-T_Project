afc6b7b82e3b0123c1547f9acd74ff90d25f5a23
cathat00 <34935679+cathat00@users.noreply.github.com>
2025-07-12 12:55:45 -0400

    Update tasks.py

--- modules/tasks.py
+++ modules/tasks.py
@@ -1,59 +1,78 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from abc import ABC, abstractmethod
 
-# ===========================
-# == VANILLA REACHING TASK ==
-# ===========================
-def create_reaching_task_stimuli(tsteps:int, pulse_steps:int, n_targets:int=6, amplitude:float=1., twod:bool=False):
-    """
-    Create the set of stimuli, which we sample from at each trial
+# ===========================================
+# == ABSTRACT BASE CLASS FOR TASK CREATION ==
+# ===========================================
+# This allows us to define common functionality
+# for all motor reaching tasks. All task classes 
+# should inherit from this class and implement the 
+# abstract methods _create_stimuli and _create_targets.
+# For more on abstract base classes in Python...
+# https://www.datacamp.com/tutorial/python-abstract-classes
 
-    Args:
-      tsteps (int):
-      pulse_steps (int):
-      n_targets (int):
-      amplitude (float):
-      twod (bool):
+class MotorReachingTask(ABC):
 
-    Returns:
-      (np.array): array of (n_targets, pulse_steps, n_targets) stimuli
-      
-    """
-    # create stimulus
-    stimulus = np.zeros((n_targets, tsteps, n_targets))
-    if twod:
-        phis = np.linspace(0,2*np.pi,targets,endpoint=False)
-        for j in range(stimulus.shape[0]):
-            stimulus[j,:pulse_length,0] = amplitude*np.cos(phis[j])
-            stimulus[j,:pulse_length,1] = amplitude*np.sin(phis[j])
-            stimulus[j,:pulse_length,2:] = 0
+  def __init__(self, tsteps:int=200, ntargets:int=6, target_max=0.02,
+               stim_length:int=20, stim_amplitude:float=1, 
+               stim_is_2d:bool=False):
+    # -- Time component
+    self.tsteps = tsteps # Timesteps
+
+    # -- Stimulus
+    self.stim_length = stim_length # Length of the pulse stimulus (ms)
+    self.stim_amplitude = stim_amplitude # Amplitude of the pulse stimulus
+    self.stim_is_2d = stim_is_2d
+
+    # -- Targets 
+    self.ntargets = ntargets # Number of reaching targets
+    self.target_max = target_max # Max X and Y coordinate of target
+
+    # -- Create task
+    # Targets shape is (ntargets, stim_length, 2D coordinates)
+    self._create_targets()
+    # Stimulus shape is (ntargets, stim_length, ntargets)
+    self._create_stimuli() 
+
+  @abstractmethod
+  def _create_stimuli(self):
+    pass
+
+  @abstractmethod
+  def _create_targets(self):
+    pass
+
+
+# =========================
+# == BASIC REACHING TASK ==
+# =========================
+# The basic reaching task, used by Feulner.
+
+class BasicReachingTask(MotorReachingTask)
+
+  def _create_stimuli(self):
+    self.stimuli = np.zeros((self.ntargets, self.tsteps, self.ntargets))
+    if self.stim_is_2d:
+        phis = np.linspace(0,2*np.pi,self.targets,endpoint=False)
+        for j in range(self.stimuli.shape[0]):
+            self.stimuli[j,:self.stim_length,0] = self.stim_amplitude*np.cos(phis[j])
+            self.stimuli[j,:self.stim_length,1] = self.stim_amplitude*np.sin(phis[j])
+            self.stimuli[j,:self.stim_length,2:] = 0
     else:
-        for j in range(n_targets):
-            stimulus[j,:pulse_steps,j] = amplitude
-    return stimulus
+        for j in range(self.ntargets):
+            self.stimuli[j,:self.stim_length,j] = self.stim_amplitude
 
-def create_reaching_task_targets(tsteps, pulse_steps, n_targets:int=6, stype='constant', target_max:float=0.2):
-    """
-    Create the set of target coordinates (6 by default) that the network
-    must reach before the end of a trial. The network starts from the center of
-    the computer screen (coordinate: (0,0))
-    
-    """
-    # create target trajectories
-    phis = np.linspace(0, 2*np.pi, n_targets, endpoint=False)
-    rs = np.zeros(tsteps)
+  def _create_targets(self):
+      phis = np.linspace(0, 2*np.pi, self.ntargets, endpoint=False)
+      rs = np.zeros(self.tsteps)
 
-    # define each target's x and y coordinate
-    rs[pulse_steps:] = np.ones(tsteps-pulse_steps)*target_max
-    traj = np.zeros((n_targets,tsteps,2))
-    for j in range(n_targets):
-
-        # create x-coordinate on screen
-        traj[j,:,0] = rs*np.cos(phis[j])
-
-        # create y-coordinate on screen
-        traj[j,:,1] = rs*np.sin(phis[j])
-    return traj
+      # define each target's x and y coordinate
+      rs[self.stim_length:] = np.ones(self.tsteps-self.stim_length)*self.target_max
+      self.targets = np.zeros((self.ntargets,self.tsteps,2))
+      for j in range(self.ntargets):
+          # create x-coordinate on screen
+          self.targets[j,:,0] = rs*np.cos(phis[j])
+          # create y-coordinate on screen
+          self.targets[j,:,1] = rs*np.sin(phis[j])
 
 # ================
 # == FORCE TASK ==
