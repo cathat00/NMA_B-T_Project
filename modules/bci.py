120fa481ecef397708faf5c08b12fe124ca0b0c2
cathat00 <34935679+cathat00@users.noreply.github.com>
2025-07-16 08:17:03 -0400

    Added configs.py, made modfules a formal python lib. (#7)

--- modules/bci.py
+++ modules/bci.py
@@ -3,7 +3,7 @@ import sklearn.linear_model as lm
 
 class BCI():
 
-  def __init__(self, network, target_max, n_output_units=2, scale=0.04, denom=0.2):
+  def __init__(self, network, target_max, n_output_units=2, n_pcs_to_select=10, scale=0.04, denom=0.2):
     """
     Create feedforward decoder from RNN to (x,y) output units
     for learning (random weights)
@@ -17,16 +17,19 @@ class BCI():
     self.decoder *= (initial_decoder_fac / np.linalg.norm(self.decoder))
     self.n_output_units = n_output_units
 
-  def train(self, inputP, target, order):
+  def train(self, proj_activity, target, order):
     """
     Train the decoder to perform the six-cue
     motor reaching task
     
     """
-    # initialize predictor neural activity
+    # Crop inputP to desired dimensionality
+    inputP = proj_activity[...,:self.n_pcs_to_select]
+
+    # Initialize predictor neural activity
     X = np.zeros((inputP.shape[0]*inputP.shape[1], inputP.shape[-1]))
 
-    # initialize predicted target
+    # Initialize predicted target
     Y = np.zeros((inputP.shape[0]*inputP.shape[1], self.n_output_units))
 
     # fill up
@@ -42,7 +45,3 @@ class BCI():
     # make predictions
     y = reg.predict(X)
     self.mse = np.mean((y-Y)**2)
-
-    # make predictions
-    self.decoder = reg.predict(X)
-    mse = np.mean((y-Y)**2)
---
+++ modules/configs.py
@@ -0,0 +1,90 @@
+import os
+import pickle
+import numpy as np
+from os.path import join
+from abc import ABC, abstractmethod
+from . import tasks
+from .network import RNN
+from .bci import BCI
+
+# ===============================================
+# == UTILITY FUNCS FOR READING / WRITING .PKLs ==
+# ===============================================
+
+def save_data(filepath, data):
+  with open(filepath, 'wb') as f:
+    return pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+def load_data(filepath):
+  with open(filepath, 'rb') as f:
+      return pickle.load(f)
+
+
+# =============================================
+# == BASE CLASS FOR EXPERIMENT CONFIGURATION ==
+# =============================================
+
+class ExperimentConfig(ABC):
+  
+  def __init__(self, ntrials=80, ntrials_manifold=50, seed=2):
+    self.ntrials = ntrials
+    self.ntrials_manifold = ntrials_manifold
+    self.random_seed = seed
+    
+    # Set random seed
+    if not seed is None:
+      np.random.seed(self.random_seed)
+
+  @abstractmethod
+  def save(out_dir, **kwargs):
+    pass
+
+
+# ====================================
+# == BASIC EXPERIMENT CONFIGURATION ==
+# ====================================
+
+class BasicExperimentConfig(ExperimentConfig):
+  
+  def __init__(self, ntargets=6, ntrials=80, ntrials_manifold=50, seed=2):
+
+    super().__init__(ntrials, ntrials_manifold, seed)
+
+    # -- The motor reaching task
+    self.task = tasks.BasicReachingTask(ntargets=ntargets)
+    # -- The recurrent neural network
+    self.rnn = RNN(N_in=self.task.ntargets, verbosity=1)
+    # -- The brain computer interface
+    self.bci = BCI(self.rnn, self.task.target_max)
+    # -- The feedback matrix
+    self.feedback = np.linalg.pinv(self.bci.decoder)
+    
+  def save(self, filepath, manifold_data=None, overwrite=False):
+
+    # Create directory path if it doesn't already exist.
+    dir_path = os.path.dirname(filepath)
+    os.makedirs(dir_path, exist_ok=True)
+
+    # If file already exists...
+    if os.path.exists(filepath):
+      if overwrite:
+        # Overwrite it
+        os.remove(filepath)
+      else:
+        # Don't overwrite it! Throw an exception.
+        raise Exception(f"File {filepath} already exists! Overwrite set to false in call to 'save'")
+
+    # Save data to .pkl
+    data = {
+      'params': {
+          'random_seed':self.random_seed,
+          'ntrials':self.ntrials,
+          'ntrials_manifold':self.ntrials_manifold,
+      },
+      'rnn':self.rnn,
+      'task':self.task,
+      'bci':self.bci,
+      'feedback':self.feedback,
+      'manifold_data':manifold_data,
+    }
+    save_data(filepath, data)
